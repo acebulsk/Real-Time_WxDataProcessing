6695f567ae4f0ab4fdc1f70b35b3c2368134c0e8
Alex Cebulski <Alex.Cebulski@viu.ca>
2019-04-10 15:58:07 -0700

    initial commit

--- convertHoboDataToHourly.R
+++ convertHoboDataToHourly.R
@@ -1,7 +1,6 @@
 # a script to resample 15 min interval HOBO logger data to 1 hour averages 
 library(xts)
 library(dplyr)
-library(chron)
 
 path = 'H:/FLNRO/Russell Creek/Data/'
 setwd(path)
@@ -11,16 +10,14 @@ apply.hourly <- function(x, FUN,...) {
   period.apply(x, ep, FUN, ...)
 }
 
-df <- read.csv('Steph 1/Steph_1 hobo data_2010 to 2014.csv') %>% 
-  slice(1:10000)
-
+df <- read.csv('Steph 1/Steph_1 hobo data_2010 to 2014.csv') 
 df <- read.zoo(df, index = 2:3, format = "%m/%d/%Y %H:%M", tz = "UTC") 
 
 # if duplicate date + times take last entry
 df <- aggregate(df, index(df), tail, 1)
 
 ts <- as.xts(df)
- gsdf afg
+
 df.hourly <-  apply.hourly(ts, mean)
 
 
